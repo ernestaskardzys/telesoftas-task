fa5e1c17efeef8c424869e88cf83a08bdfd1bb14
Ernestas Kardzys <ernestas.kardzys@gmail.com>
2018-08-06 09:12:03 +0100

    Initial commit

---
+++ src/main/java/info/ernestas/gildedrose/GildedroseApplication.java
@@ -0,0 +1,12 @@
+package info.ernestas.gildedrose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GildedroseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GildedroseApplication.class, args);
+	}
+}
